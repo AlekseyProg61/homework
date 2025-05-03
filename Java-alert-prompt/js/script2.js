7fe56fe77b21f8dcb3710d9c18c7b48f3799de34
Aleksey Kashirin <kav-88@yandex.ru>
2025-05-03 15:45:08 +0300

    03052025

--- Java-alert-prompt/js/script2.js
+++ Java-alert-prompt/js/script2.js
@@ -1,8 +1,23 @@
 document.getElementById("sumButton").addEventListener("click", function () {
   let UserNum1 = prompt("Введите первое число диапазона");
   let UserNum2 = prompt("Введите второе число диапазона");
-  sum = 0;
-  for (let i = UserNum1; i <= UserNum2; i++) sum = sum + i;
-  result = Math.floor(sum + sum);
-  alert(`Диапазон ${sum}. Сумма диапазона ${result}`);
+  if ((UserNum1 > UserNum2, UserNum2, (UserNum1 = UserNum1), UserNum2)) sum = 0;
+  a = 0;
+  while (a < UserNum2) {
+    a++;
+    sum += a;
+  }
+  alert(`Сумма всех чисел диапазона от ${UserNum1} до ${UserNum2} = ${sum}`);
+});
+document.getElementById("dividerButton").addEventListener("click", function () {
+  let UserNum1 = prompt("Введите первое число ");
+  let UserNum2 = prompt("Введите второе число ");
+  UserNum1 = Math.abs(UserNum1);
+  UserNum2 = Math.abs(UserNum2);
+  while (a != 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  alert(`${a}`);
 });
